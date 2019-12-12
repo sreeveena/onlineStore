2f7defd288331d5d1da581c7f43a0951af4eb805
sreeveena <sreeveena21@gmail.com>
2019-12-11 17:29:36 -0800

    installed the dependencies.

---
+++ bamazon_customer.js
@@ -0,0 +1,24 @@
+var inquirer = require('inquirer');
+
+var mysql = require("mysql");
+
+var connection = mysql.createConnection({
+  host: "localhost",
+
+  // Your port; if not 3306
+  port: 3306,
+
+  // Your username
+  user: "root",
+
+  // Your password
+  password: "password",
+  database: "bamazonDB"
+});
+
+connection.connect(function(err) {
+  if (err) throw err;
+  console.log("connected as id " + connection.threadId + "\n");
+  // readProduct(data);
+  promptUserOption();
+});
