54830e5aaffaa89403dcc9e98f72e74ee9086e2c
sreeveena <sreeveena21@gmail.com>
2019-12-15 10:35:28 -0800

    table display

--- bamazon_customer.js
+++ bamazon_customer.js
@@ -1,17 +1,27 @@
 var inquirer = require('inquirer');
-
+var Table = require('cli-table');
 var mysql = require("mysql");
 
+var table = new Table({
+    chars: {
+        'top': '─', 'top-mid': '┬', 'top-left': '┌', 'top-right': '┐'
+      , 'bottom': '─', 'bottom-mid': '┴', 'bottom-left': '└', 'bottom-right': '┘'
+      , 'left': '│', 'left-mid': '├'
+      , 'mid': '─', 'mid-mid': '┼'
+      , 'right': '│', 'right-mid': '┤'
+      , 'middle': '│'
+    },
+    head: ['ID', 'product_name','department_name', 'price', 'stock_quantity'],
+    colWidths: [6, 40, 15, 15, 30],
+    // style : {'padding-left': 1,
+    // 'padding-right': 1,
+  
+    border: ['black']
+});
 var connection = mysql.createConnection({
   host: "localhost",
-
-  // Your port; if not 3306
   port: 3306,
-
-  // Your username
   user: "root",
-
-  // Your password
   password: "password",
   database: "bamazonDB"
 });
@@ -19,6 +29,38 @@ var connection = mysql.createConnection({
 connection.connect(function(err) {
   if (err) throw err;
   console.log("connected as id " + connection.threadId + "\n");
-  // readProduct(data);
-  promptUserOption();
+    displayItems(function(returnValue){
+        table.push(
+            // displayItems()
+            returnValue
+        );
+    });
+
+   console.log(table.toString());
 });
+
+function displayItems(cb){
+    connection.query("SELECT * FROM products", function(err, res) {
+        if (err) throw err;
+        console.log(res);
+            cb(res);
+        connection.end();
+      });
+}
+
+// function promptCustomer(){
+//     inquirer
+//     .prompt({
+//       name: "product",
+//       type: "input",
+//       message: "What is the ID of the item you would like to purchase?"
+//     },
+//     {
+//         name: "quantity",
+//         type: "input",
+//         message: "How any would you like?"   
+//     }).then (function(id){
+//         if(id.product === )
+
+//     });
+// }
